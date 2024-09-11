b9ae856b11c9ef699ba97a070eb1f23ff0220b99
ayto <asidzharimova@gmail.com>
2024-09-11 18:10:30 +0200

    Use ubuntu as provider, move values to variables

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# export k3s environment variables
+export INSTALL_K3S_EXEC=""
+
+# isntall curl if not installed
+
+# install k3s
 curl -sfL https://get.k3s.io | sh -
 
-echo "Hello"
+echo "Finished server setup"
